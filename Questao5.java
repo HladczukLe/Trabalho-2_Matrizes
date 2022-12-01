130c1b73b6f7dbddfa2603f354820d15e564c3df
Letícia Hladczuk <109438911+HladczukLe@users.noreply.github.com>
2022-12-01 16:39:18 -0300

    Add files via upload

---
+++ Questao5.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+
+public class Questao5 {
+    public static void main(String[] args) {
+        Random valor = new Random();
+        // int dimensao = valor.nextInt(3);
+        
+        int [][] matriz = new int[3][3];
+        int determinante=0;
+        for (int i = 0; i<matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                matriz[i][j] = valor.nextInt(10);
+            }
+        }
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.print(matriz[i][j]+"\t");
+
+            }
+            System.out.println(" ");
+        }
+        
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                determinante = (matriz[0][0]*((matriz[1][1]*matriz[2][2])+(-matriz[1][2]*matriz[2][1]))) + (-matriz[1][0]*((matriz[0][1]*matriz[2][2])+(-matriz[0][2]*matriz[2][1]))) + (matriz[2][0]*((matriz[0][1]*matriz[1][2])+(-matriz[0][2]*matriz[1][1])));
+                
+            }
+        }
+        System.out.print("Determinante da matriz: "+determinante);
+    }
+}
