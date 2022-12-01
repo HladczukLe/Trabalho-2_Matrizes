8e7dcd2973e70d54ff3c9a76c972205fb98e4850
Letícia Hladczuk <109438911+HladczukLe@users.noreply.github.com>
2022-12-01 16:44:48 -0300

    Update Questao5.java

--- Questao5.java
+++ Questao5.java
@@ -25,6 +25,7 @@ public class Questao5 {
                 determinante = matriz[0][0]*((matriz[1][1]*matriz[2][2])-(matriz[1][2]*matriz[2][1])) + 
                     (-matriz[1][0]*((matriz[0][1]*matriz[2][2])-(matriz[0][2]*matriz[2][1]))) + 
                     matriz[2][0]*((matriz[0][1]*matriz[1][2])-(matriz[0][2]*matriz[1][1]));
+                
             }
         }
         System.out.print("Determinante da matriz: "+determinante);
